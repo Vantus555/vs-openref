78cf0589a91edddce8fcfd42d36528906e1ae7bf
Vantus555 <nindzay1998@gmail.com>
2020-11-18 22:48:28 +0300

    design user, roles and root begin

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index.as_view(), name="user_url"),
+    path('roles_and_roots', roles_and_roots.as_view(), name="roles_and_roots_url"),
 ]
--- user/views.py
+++ user/views.py
@@ -5,7 +5,12 @@ from django.views.generic import View
 
 class index(View):
     def get(self, request):
-        if 'login' not in request.session and not request.GET.get('reg', False):
-            return render(request, 'user/input.html', context={'path' : request.get_full_path()})
-        elif request.GET.get('reg', None):
-            return render(request, 'user/reg.html', context={'path' : request.get_full_path()})
+        return render(request, 'user/admin.html', context={'path' : request.get_full_path()})
+        # if 'login' not in request.session and not request.GET.get('reg', False):
+        #     return render(request, 'user/input.html', context={'path' : request.get_full_path()})
+        # elif request.GET.get('reg', None):
+        #     return render(request, 'user/reg.html', context={'path' : request.get_full_path()})
+
+class roles_and_roots(View):
+    def get(self, request):
+        return render(request, 'user/roles_and_roots.html', context={'path' : request.get_full_path()})
